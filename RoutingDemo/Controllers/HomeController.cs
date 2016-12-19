fe4bd34eda5db81a321e3bc6436eacfd7cc6888f
Santroy <krytront@gmail.com>
2016-12-19 02:04:06 +0100

    Add project files.

---
+++ RoutingDemo/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RoutingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            var r = RouteTable.Routes.GetVirtualPath(null, new RouteValueDictionary(new { controller = "Kontroler", action = "Akcja", id = "12", parametr = "Bartek" }));
+
+            string s = r.VirtualPath;
+
+            string result = Server.HtmlEncode("<script></script>");
+            result += "<br>";
+
+            foreach (string routekey in RouteData.Values.Keys)
+            {
+                //result += "Key: " + routekey + " Value: " + RouteData.Values[routekey] as string;
+                //result += "<br>";
+
+                result += string.Format("Key: {0} Value: {1}", routekey, RouteData.Values[routekey] as string);
+                result += "<br>";
+
+            
+            }
+            return result + "\n" + s;
+        }
+    }
+}
---
+++ RoutingDemo/Controllers/StoreController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoutingDemo.Controllers
+{
+    [RoutePrefix("prefiks")]
+    public class StoreController : Controller
+    {
+        // GET: Store
+        [Route("to/jest/{moja}/trasa")]
+        public string Index(string moja)
+        {
+            return "Attribute routing";
+        }
+
+        [Route("{param:int}")]
+        public string Attr(string param) {
+            return param;
+        }
+
+    }
+}
